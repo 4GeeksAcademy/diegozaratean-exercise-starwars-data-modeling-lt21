708da94a3d5ba98fc7d76ce71769cf19fbd7cf22
diego zarate <diegozaratean@gmail.com>
2023-10-11 14:48:38 +0000

    finish class

--- src/models.py
+++ src/models.py
@@ -7,26 +7,53 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+
+class Estudio(Base):
+    __tablename__ = 'estudio'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+    nombre = Column(String(250), nullable=False)
+    logo = Column(String(250), nullable=False)
+    slogan = Column(String(250), nullable=False)
+			
+class ModoJuego(Base):
+    __tablename__ = 'modo_juego'
+    # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    nombre = Column(String(250), nullable=False)
 
-    def to_dict(self):
-        return {}
+class Videojuego(Base):
+    __tablename__ = 'videojuego'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(250), nullable=False)
+    genero = Column(String(250), nullable=False)
+    año_lanzmaineto = Column(String(250), nullable=False)
+    estudio_id = Column(Integer, ForeignKey('estudio.id'))
+    estudio = relationship(Estudio)
+    modo_juego_id = Column(Integer, ForeignKey('modo_juego.id'))
+    modo_juego = relationship(ModoJuego)
+
+class Consola(Base):
+    __tablename__ = 'consola'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(250), nullable=False)
+
+
+class VideojuegoConsola(Base):
+    __tablename__ = 'videojuego_consola'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    consola_id = Column(Integer, ForeignKey('consola.id'))
+    consola = relationship(Consola)
+    videojuego_id = Column(Integer, ForeignKey('videojuego.id'))
+    videojuego = relationship(Videojuego)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
